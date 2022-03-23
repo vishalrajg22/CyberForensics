8a7ca345d6c5a01fd3e7518467cb108bf880e994
vishalrajg22 <40936134+vishalrajg22@users.noreply.github.com>
2022-03-23 11:05:07 +0530

    Hash Generator Updated

--- A0/Hash_Generator.py
+++ A0/Hash_Generator.py
@@ -51,20 +51,47 @@ for file in file_list:
         path_li.append(main_dir + file + "/")
 
 i = 0
+data_li = []
+data_types = []
+data_state = []
 for path in path_li:
     file_list = get_files(path)
     for file in file_list:
-        i += 1
+        dict = {}
+        # i += 1
         file_path = path + file
         file_size = os.path.getsize(file_path)
-        print("S. No:",i)
-        print("File/State:", "empty")
-        print("Name:", file)
-        print("Filetype:",file.split(".")[1])
-        print("File size:",file_size,"bytes")
-        print("MD5:", md5(file_path))
-        print("SHA1:", sha1(file_path))
-        print("SHA256:", sha256(file_path))
-        print("SHA512:", sha512(file_path))
-        print("NTLM:",ntlm(file_path))
-        print()
+        # print("S. No:",i)
+        dict["File/State"] = path[2:-1]
+        dict["Name"] = file
+        dict["Filetype"] = file.split(".")[1]
+        dict["File size"] = str(file_size) + " bytes"
+        dict["MD5"] = md5(file_path)
+        dict["SHA1"] = sha1(file_path)
+        dict["SHA256"] = sha256(file_path)
+        dict["SHA512"] = sha512(file_path)
+        dict["NTLM"] = ntlm(file_path)
+        data_li.append(dict)
+        data_types.append(file.split(".")[1])
+        data_state.append(path[2:-1])
+
+data_types = sorted(list(set(data_types)))
+data_state = sorted(list(set(data_state)))
+
+i = 0
+for dt in data_types:
+    for state in data_state:  
+        for data in data_li:
+            if data["Filetype"] == dt and data["File/State"] == state :
+                i += 1
+                print("S. No:",i)
+                print("File/State:", data["File/State"])
+                print("Name:", data["Name"])
+                print("Filetype:", data["Filetype"])
+                print("File size:", data["File size"])
+                print("MD5:", data["MD5"])
+                print("SHA1:", data["SHA1"])
+                print("SHA256:", data["SHA256"])
+                print("SHA512:", data["SHA512"])
+                print("NTLM:", data["NTLM"])
+                print()
